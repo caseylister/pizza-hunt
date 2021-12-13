a92b4c1f0090e1038f7dcadb23adedc9fc0378df
caseylister <caseynlister@gmail.com>
2021-12-13 15:42:52 -0500

    add-pizza api/front-end

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const apiRoutes = require('./api');
+const htmlRoutes = require('./html/html-routes');
+
+
+// add prefix of /api to all of api routes
+router.use('/api', apiRoutes);
+router.use('/', htmlRoutes);
+
+router.use((req, res) => {
+  res.status(404).send('<h1>😝 404 Error!</h1>');
+});
+
+module.exports = router;
